b8346010a73e62dbfb6ce899e5fe859b510df99f
江雨 <1658210374@qq.com>
2025-12-19 15:57:18 +0800

    fix(vue):实现html与vue数据的通信

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,27 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref,onBeforeUnmount } from "vue";
 
 defineProps({
   msg: String,
-})
+});
+const count = ref(0);
 
-const count = ref(0)
+onMounted(() => {
+  // 监听来自Flutter的消息
+  window.addEventListener("updateVueData", updateData);
+});
+
+onBeforeUnmount(() => {
+  // 监听来自Flutter的消息
+  window.removeEventListener("updateVueData", updateData);
+});
+
+function updateData(data) {
+  count.value = data.detail;
+}
+function sendToFlutter() {
+  window.flutterBridge.postMessage("Hello from JavaScript!");
+}
 </script>
 
 <template>
@@ -13,27 +29,10 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <button @click="sendToFlutter()">
+    Send Message to Flutter And Get Flutter Data
+  </button>
 </template>
 
 <style scoped>
